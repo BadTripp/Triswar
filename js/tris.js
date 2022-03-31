ca19ba676889eabcba4d593132efb090d271d284
BadTripp <renato.turetta00@gmail.com>
2022-03-31 22:44:26 +0200

    triss-js

---
+++ js/tris.js
@@ -0,0 +1,33 @@
+
+
+//                                              0  1  2
+//                                              3  4  5
+//                                              6  7  8
+//  Tris win 
+//  048
+//  246
+//  036
+//  147
+//  258
+//  012
+//  345
+//  678
+
+
+
+function playerClick(casellaCliccata){  
+     
+
+    
+}
+
+
+function computerMove(){
+    scriviCasellaO();
+}
+function scriviCasellaX(casellaCliccata){
+    document.getElementById(casellaCliccata).innerText="X";
+}
+function scriviCasellaO(casellaCliccata){
+    document.getElementById(casellaCliccata).innerText="O";
+}
